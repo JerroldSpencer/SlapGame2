151f79be274bc07e9bf17a55bbb950364bc3276b
Jerrold Spencer <2abc2xyz7a6b@gmail.com>
2019-08-20 14:03:12 -0600

    created entry point and button

---
+++ app/main.js
@@ -0,0 +1,10 @@
+import GameController from './components/game-controller.js';
+
+class App {
+  constructor() {
+    this.controllers = {
+      gameController: new GameController()
+    }
+  }
+}
+window.app = new App()
